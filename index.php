aef1d105981fe503a4945f206c3f062a42e496ce
Екатерина Листюхина <lisekater247@gmail.com>
2021-12-05 21:09:07 +0300

    add plus

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>Хай!</h1>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Моя страничка </title>
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Результат сложения 20110212 и 21201102 равен "
+				echo 20110212+21201102;
+			?>
+		</div>
+	</body>
+</html>
